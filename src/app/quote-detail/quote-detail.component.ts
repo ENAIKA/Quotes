cdc7ed62ab49c21325fc3cfddbbe9908c3829033
ENAIKA <naikaesther5@gmail.com>
2020-04-02 01:21:31 +0300

    add  EventEmitter

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {Quote}from '../quote';
 @Component({
   selector: 'app-quote-detail',
   templateUrl: './quote-detail.component.html',
   styleUrls: ['./quote-detail.component.css']
 })
 export class QuoteDetailComponent implements OnInit {
+  @Input() quote: Quote;
+  @Output() isDeleted = new EventEmitter<boolean>();
+  quoteDelete(deleted:boolean){
+    this.isDeleted.emit(deleted);
+  }
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
