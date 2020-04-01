166bc2424f452adde58f29b84c7af1e61c9c5a36
ENAIKA <naikaesther5@gmail.com>
2020-04-02 01:15:30 +0300

    add submit()

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,11 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
+import {Quote} from '../quote';
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+  newQuote = {id:0,quote:"",author:"",person:"",datePosted:new Date()};
+  @Output() addQuote = new EventEmitter<Quote>();
+
+  submitQuote(){
+this.addQuote.emit(this.newQuote);
+this.newQuote = {id:0,quote:"",author:"",person:"",datePosted:new Date()};;
+  }
 
   constructor() { }
 
