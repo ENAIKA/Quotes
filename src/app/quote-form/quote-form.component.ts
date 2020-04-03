1e194bb3f1747bd7605a9363acca7182657da8c1
ENAIKA <naikaesther5@gmail.com>
2020-04-03 14:58:43 +0300

    disable submit button on invalid form

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 import {Quote} from '../quote';
+import { FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
-  newQuote = {id:0,quote:"",author:"",person:"",datePosted:new Date()};
+  newQuote = {id:0,quote:"",author:"",person:"",datePosted:new Date(),upVote:0,downVote:0};
   @Output() addQuote = new EventEmitter<Quote>();
-
+quoteform:FormGroup;
   submitQuote(){
 this.addQuote.emit(this.newQuote);
-this.newQuote = {id:0,quote:"",author:"",person:"",datePosted:new Date()};;
+alert('Quote added successfully. Scroll down to see!');
+this.newQuote = {id:0,quote:"",author:"",person:"",datePosted:new Date(),upVote:0,downVote:0};
   }
 
   constructor() { }
